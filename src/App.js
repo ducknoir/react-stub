bd0ddc749731fc1f830e1917f16b76ef93323375
ducknoir <ducknoir@users.noreply.github.com>
2024-04-05 19:58:45 -0700

    Add div

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 function App()
 {
     return 
-        [
-            <h1>Hi Diane and Mindy!</h1>,
-            <p></p>,
-            <h1>And Fay!</h1>
-        ];
+        (
+            <div>
+                <h1>Hi Diane and Mindy!</h1>
+                <p></p>
+                <h1>And Fay!</h1>
+            </div>
+        );
 }
 
 export default App
