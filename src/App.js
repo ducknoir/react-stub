fcba396626ee366981823086dfe3a9ba9bc41f6b
ducknoir <ducknoir@users.noreply.github.com>
2024-04-05 19:54:27 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@
 function App()
 {
     return 
-        ([
+        [
             <h1>Hi Diane and Mindy!</h1>,
             <p></p>,
             <h1>And Fay!</h1>
-        ]);
+        ];
 }
 
 export default App
