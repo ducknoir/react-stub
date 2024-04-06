60e407e964ec4015199e9d748ce6df388ac5df33
ducknoir <ducknoir@users.noreply.github.com>
2024-04-05 19:47:03 -0700

    Fay

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 
 function App()
 {
-    return <h1>Hi Diane and Mindy!</h1>;
+    return 
+        (
+            <h1>Hi Diane and Mindy!</h1>
+            <br>
+            <h1>And Fay!</h1>
+        );
 }
 
 export default App
